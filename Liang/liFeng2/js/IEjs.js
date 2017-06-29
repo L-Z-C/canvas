375498737c7b721b622834865b2991f69bfe54be
liang <765535825@qq.com>
2017-06-29 10:58:07 +0800

    chat for weiXin

---
+++ Liang/liFeng2/js/IEjs.js
@@ -0,0 +1,109 @@
+/**
+ * Created by Liulc on 2017/6/27.
+ */
+if(!document.getElementsByClassName){
+    document.getElementsByClassName = function(className, element){
+        var children = (element || document).getElementsByTagName('*');
+        var elements = new Array();
+        for (var i=0; i<children.length; i++){
+            var child = children[i];
+            var classNames = child.className.split(' ');
+            for (var j=0; j<classNames.length; j++){
+                if (classNames[j] == className){
+                    elements.push(child);
+                    break;
+                }
+            }
+        }
+        return elements;
+    };
+}
+function getEleChild(ele){
+    var eleChildArr=ele.children || ele.childNodes;
+    var childArrTmp=[];
+    for(var i=0;i<eleChildArr.length;i++){
+        if(eleChildArr[i].nodeType==1){
+            childArrTmp.push(eleChildArr[i]);
+        }
+    }
+    return childArrTmp;
+}
+function getClassNames(classStr, tagName) {
+    if (document.getElementsByClassName) {
+        return document.getElementsByClassName(classStr)
+    } else {
+        var nodes = document.getElementsByTagName(tagName), ret = [];
+        for (var i = 0; i < nodes.length; i++) {
+            if (hasClass(nodes[i], classStr)) {
+                ret.push(nodes[i])
+            }
+        }
+        return ret;
+    }
+}
+function hasClass(tagStr, classStr) {
+    var arr = tagStr.className.split(/\s+/);
+    for ( var i = 0; i < arr.length; i++)
+    {
+        if (arr[i] == classStr)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+function getEleStyle(ele, cssName){
+
+    var style = window.getComputedStyle ?
+
+        window.getComputedStyle(ele, "") :
+
+        ele.currentStyle;
+
+    return style[cssName];
+
+}
+window.onload = function () {
+    var oContent = document.getElementById('content'),aFirstThree = getClassNames('firstThree','ul');
+    oContent.style.marginLeft = '-'+oContent.offsetWidth/2+'px';
+    aFirstThree[0].style.marginLeft = '-40%';
+    var aUsersLi = document.getElementsByClassName('usersLi');
+    function progress(obj,objs,objes) {
+        for (var i =0 ;i<aUsersLi.length;i++){
+            var oBusinessBar = getEleChild(aUsersLi[i])[obj];
+            var small = getEleChild(oBusinessBar)[1];
+            var lager = getEleChild(oBusinessBar)[2];
+            var oBackground = getEleChild(oBusinessBar)[0];
+            var oActual = getEleChild(oBusinessBar)[3].innerHTML;
+            var num = parseFloat((oActual -small.innerHTML)/(lager.innerHTML-small.innerHTML));
+            var aaa = num*oBusinessBar.offsetHeight;
+            oBackground.style.height = aaa+'px';
+            small.innerHTML = small.innerHTML.replace(/0000/,"")+'w';
+            lager.innerHTML = lager.innerHTML.replace(/0000/,"")+'w';
+            (function (i) {
+                var oBusinessBars = getEleChild(aUsersLi[i])[objs];
+                oBusinessBars.attachEvent('onmouseover',function(){
+                    var oShowActual = getEleChild(getEleChild(getEleChild(aUsersLi[i])[0])[1])[3];
+                    oShowActual.innerHTML = getEleChild(oBusinessBar)[3].innerHTML;
+                    oShowActual.style.display = 'block';
+                    getEleChild(getEleChild(aUsersLi[i])[objes])[0].style.display = 'block';
+                    oShowActual.style.marginLeft = '-'+oShowActual.offsetWidth/2+'px';
+                },false);
+                oBusinessBars.attachEvent('onmouseout',function(){
+                    getEleChild(getEleChild(getEleChild(aUsersLi[i])[0])[1])[3].style.display = 'none';
+                    getEleChild(getEleChild(aUsersLi[i])[objes])[0].style.display = 'none';
+                },false);
+            })(i);
+        }
+    }
+    progress(4,2,2);
+    progress(5,3,3);
+    aUsersLi[1].style.border = 'solid 1px #8c8c8c';
+    aUsersLi[1].style.height = '218px';
+    aUsersLi[2].style.border = 'solid 1px #D98719';
+    aUsersLi[2].style.height = '218px';
+    var aDl = oContent.getElementsByTagName('dl');
+    for (var i=0;i<aDl.length;i++){
+        aDl[i].style.marginLeft = '-'+aDl[i].offsetWidth/2+'px';
+    }
+};
--- Liang/liFeng2/js/js.js
+++ Liang/liFeng2/js/js.js
@@ -1,34 +1,36 @@
 /**
  * Created by Liulc on 2017/6/15.
  */
-window.onload =  ()=>{
-    let aUsersLi = document.querySelectorAll('.usersLi');
+window.onload =  function(){
+    var aUsersLi = document.getElementsByClassName('usersLi');
     function progress(obj,objs) {
-        for (let i =0 ;i<aUsersLi.length;i++){
-            let oBusinessBar = aUsersLi[i].querySelector(obj);
-            let small = oBusinessBar.querySelector('bdo');
-            let lager = oBusinessBar.querySelector('bdi');
-            let oBackground = oBusinessBar.querySelector('.background');
-            let oActual = oBusinessBar.querySelector('.actual').innerHTML;
-            let num = parseFloat((oActual -small.innerHTML)/(lager.innerHTML-small.innerHTML));
-            let aaa = num*oBusinessBar.offsetHeight;
+        for (var i =0 ;i<aUsersLi.length;i++){
+            var oBusinessBar = aUsersLi[i].getElementsByClassName(obj)[0];
+            var small = oBusinessBar.getElementsByTagName('bdo')[0];
+            var lager = oBusinessBar.getElementsByTagName('bdi')[0];
+            var oBackground = oBusinessBar.getElementsByClassName('background')[0];
+            var oActual = oBusinessBar.getElementsByClassName('actual')[0].innerHTML;
+            var num = parseFloat((oActual -small.innerHTML)/(lager.innerHTML-small.innerHTML));
+            var aaa = num*oBusinessBar.offsetHeight;
             oBackground.style.height = aaa+'px';
             small.innerHTML = small.innerHTML.replace(/0000/,"")+'w';
             lager.innerHTML = lager.innerHTML.replace(/0000/,"")+'w';
-            let oBusinessBars = aUsersLi[i].querySelector(objs);
-            oBusinessBars.addEventListener('mouseover',function(){
-                "use strict";
-                aUsersLi[i].querySelector('.showActual').innerHTML = oBusinessBar.querySelector('.actual').innerHTML;
-                aUsersLi[i].querySelector('.showActual').style.display = 'block';
-                this.querySelector('bdo').style.display = 'block';
-            },false);
-            oBusinessBars.addEventListener('mouseout',function(){
-                "use strict";
-                aUsersLi[i].querySelector('.showActual').style.display = 'none';
-                this.querySelector('bdo').style.display = 'none';
-            },false);
+            (function (i) {
+                var oBusinessBars = aUsersLi[i].getElementsByClassName(objs)[0];
+                oBusinessBars.addEventListener('mouseover',function(){
+                    "use strict";
+                    aUsersLi[i].getElementsByClassName('showActual')[0].innerHTML = oBusinessBar.getElementsByClassName('actual')[0].innerHTML;
+                    aUsersLi[i].getElementsByClassName('showActual')[0].style.display = 'block';
+                    this.getElementsByTagName('bdo')[0].style.display = 'block';
+                },false);
+                oBusinessBars.addEventListener('mouseout',function(){
+                    "use strict";
+                    aUsersLi[i].getElementsByClassName('showActual')[0].style.display = 'none';
+                    this.getElementsByTagName('bdo')[0].style.display = 'none';
+                },false);
+            })(i);
         }
     }
-    progress('.businessBar','.businessBars');
-    progress('.praiseBar','.praiseBars');
+    progress('businessBar','businessBars');
+    progress('praiseBar','praiseBars');
 };
